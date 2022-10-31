69127329bda83efd74f83298283019d16db34bb1
Christopher Brandt <christopherlgbrandt@gmail.com>
2022-10-30 18:36:48 -0600

    Removing Horspool

--- assignment3.cpp
+++ assignment3.cpp
@@ -118,60 +118,8 @@ void KMP(const char* file_contents, const char* pattern, int m, int n)
     auto diff = std::chrono::high_resolution_clock::now() - start;
     auto t1 = std::chrono::duration_cast<std::chrono::nanoseconds>(diff);
     cout << "KMP: ";
-    cout << "Number of occurrences in the text is: " << kmpmatches << " - ";
-    cout << "Number of Comparisons: " << kmpcomparisons << " - ";
-    cout << "Time: " << t1.count() << "milliseconds" << " - ";
+    cout << "Number of occurrences in the text is: " << kmpmatches << " - " << endl;
+    cout << "Number of Comparisons: " << kmpcomparisons << " - " << endl;
+    cout << "Time: " << t1.count() << " milliseconds" << " - " << endl;
 }
-/*
-void Horspool(const char* file_contents, const char* pattern, int m, int n)
-{
-    
 
- 
-     Fill the bad character array by calling
-    the preprocessing function badCharHeuristic()
-    for given pattern 
-    
- 
-    int s = 0; // s is shift of the pattern with
-                // respect to text
-    while(s <= (n - m))
-    {
-        int j = m - 1;
- 
-        /* Keep reducing index j of pattern while
-        characters of pattern and text are
-        matching at this shift s 
-        while(j >= 0 && pat[j] == txt[s + j])
-            j--;
- 
-        /* If the pattern is present at current
-        shift, then index j will become -1 after
-        the above loop
-        if (j < 0)
-        {
-            cout << "pattern occurs at shift = " <<  s << endl;
- 
-            /* Shift the pattern so that the next
-            character in text aligns with the last
-            occurrence of it in pattern.
-            The condition s+m < n is necessary for
-            the case when pattern occurs at the end
-            of text 
-            s += (s + m < n)? m-badchar[txt[s + m]] : 1;
- 
-        }
- 
-        else
-            /* Shift the pattern so that the bad character
-            in text aligns with the last occurrence of
-            it in pattern. The max function is used to
-            make sure that we get a positive shift.
-            We may get a negative shift if the last
-            occurrence of bad character in pattern
-            is on the right side of the current
-            character. 
-            s += max(1, j - badchar[txt[s + j]]);
-    }
-}
-*/
